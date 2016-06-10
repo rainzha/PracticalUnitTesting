89cc0e04361b26e1fc72448eb89b9735476f9e85
rainzha <rainzha@163.com>
2016-06-10 17:53:11 +0800

    完成3.6. Parameterized Tests.

---
+++ src/test/java/org/rainzha/practicalunittesting/MoneyParameterizedTest.java
@@ -0,0 +1,27 @@
+package org.rainzha.practicalunittesting;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
+public class MoneyParameterizedTest {
+    private static final Object[] getMoney() {
+        return new Object[]{
+                new Object[]{10, "USD"},
+                new Object[]{20, "EUR"}
+        };
+    }
+
+    @Test
+    @Parameters(method = "getMoney")
+    public void constructorShouldSetAmountAndCurrency(int amount, String currency) {
+        Money money = new Money(amount, currency);
+
+        assertThat(money.getAmount()).isEqualTo(amount);
+        assertThat(money.getCurrency()).isEqualTo(currency);
+    }
+}
