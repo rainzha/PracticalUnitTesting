83381ef15a112d6aacac0732ff80c2bc0ec29a4a
rainzha <rainzha@163.com>
2016-06-10 18:42:00 +0800

    完成3.7. Checking Expected Exceptions.

---
+++ src/test/java/org/rainzha/practicalunittesting/MoneyIAETest.java
@@ -0,0 +1,34 @@
+package org.rainzha.practicalunittesting;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitParamsRunner.class)
+public class MoneyIAETest {
+    private final static int VALID_AMOUNT = 5;
+    private final static String VALID_CURRENCY = "USD";
+
+    private static final Object[] getInvalidAmount() {
+        return new Integer[][]{{-12387}, {-5}, {-1}};
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Parameters(method = "getInvalidAmount")
+    public void constructorShouldThrowIAEForInvalidAmount(
+            int invalidAmount) {
+        new Money(invalidAmount, VALID_CURRENCY);
+    }
+
+    private static final Object[] getInvalidCurrency() {
+        return new String[][]{{null}, {""}};
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Parameters(method = "getInvalidCurrency")
+    public void constructorShouldThrowIAEForInvalidCurrency(
+            String invalidCurrency) {
+        new Money(VALID_AMOUNT, invalidCurrency);
+    }
+}
